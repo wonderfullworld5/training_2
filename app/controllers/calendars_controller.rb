8267f974ec4a0a88fa6439492ba46c2ce9e1db6a
wonderfullworld5 <bbb893576@gmail.com>
2024-01-21 10:44:48 +0900

    Issue1

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -29,6 +29,12 @@ class CalendarsController < ApplicationController
 
     plans = Plan.where(date: @todays_date..@todays_date + 6)
 
-    @week_days.push(month: (@todays_date + x).month, date: (@todays_date + x).day, plans: today_plans)
+  
+      7.times do |x|
+        today_plans = plans.select { |plan| plan.date == @todays_date + x }.map(&:plan)
+        days = { month: (@todays_date + x).month, date: (@todays_date + x).day, plans: today_plans }
+        @week_days.push(days)
     end
   end
+end
+
