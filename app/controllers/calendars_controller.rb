06d15573ace8a092973601991f4ba59272c99e9c
wonderfullworld5 <bbb893576@gmail.com>
2024-01-24 07:14:56 +0900

    Issue6

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -2,7 +2,7 @@ class CalendarsController < ApplicationController
 
   # １週間のカレンダーと予定が表示されるページ
   def index
-    getWeek
+    get_week
     @plan = Plan.new
   end
 
@@ -18,9 +18,8 @@ class CalendarsController < ApplicationController
     params.require(:calendars).permit(:date, :plan)
   end
 
-  def getWeek
+  def get_week
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
-
 
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
     @todays_date = Date.today
@@ -30,13 +29,17 @@ class CalendarsController < ApplicationController
 
     plans = Plan.where(date: @todays_date..@todays_date + 6)
 
-    wday_num = (@todays_date + x).wday # wdayメソッドを用いて取得した数値
-
-    if wday_num >= 7#「wday_numが7以上の場合」という条件式
-      wday_num -= 7
+    7.times do |x|
+      today_plans = plans.select { |plan| plan.date == @todays_date + x }.map(&:plan)
+    
+      wday_num = (@todays_date + x).wday
+    
+      if wday_num >= 7
+        wday_num = wday_num - 7
+      end
+    
+      days = { month: (@todays_date + x).month, date: (@todays_date + x).day, plans: today_plans, wday: Date::DAYNAMES[wday_num]}
+      @week_days.push(days)
     end
-    
-    @week_days.push(month: (@todays_date + x).month, date: (@todays_date + x).day, plans: today_plans)
-    
   end
-  end
+end
--- app/views/calendars/controller.rb
+++ app/views/calendars/controller.rb
@@ -1,5 +1,5 @@
 def create
-  binding.pry
+
   Plan.create(plan_params)
   redirect_to action: :index
 end
