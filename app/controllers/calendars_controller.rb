f44d302fab386e1ef0404904861f178040c59fd3
wonderfullworld5 <bbb893576@gmail.com>
2024-01-24 09:46:07 +0900

    Issue6

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -13,31 +13,38 @@ class CalendarsController < ApplicationController
   end
 
   private
+
   def plan_params
     params.require(:calendars).permit(:date, :plan)
   end
 
   def get_week
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
+
     def japanese_weekday_name(wday_num)
-      weekday_names = ["日", "月", "火", "水", "木", "金", "土"]
+      weekday_names = %w(日 月 火 水 木 金 土)
       weekday_names[wday_num]
     end
 
+    # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
     @todays_date = Date.today
-
+    # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
+    
     @week_days = []
 
     plans = Plan.where(date: @todays_date..@todays_date + 6)
 
-    7.times do |x|
-      today_plans = plans.select { |plan| plan.date == @todays_date + x }.map(&:plan)
+      7.times do |x|
+        today_plans = plans.select { |plan| plan.date == @todays_date + x }.map(&:plan) 
+      end
+         wday_num = (@todays_date + x).wday
+      if wday_num >= 7
+        wday_num = wday_num -7
+      end
 
-      wday_num = (@todays_date + x).wday
-      wday_num = 0 if wday_num == 7 # wdayが7の場合、0に修正する
-
-      days = { month: (@todays_date + x).month, date: (@todays_date + x).day, plans: today_plans, wday:japanese_weekday_name(wday_num)}
-      @week_days.push(days)
+        days = { month: (@todays_date + x).month, date: (@todays_date + x).day, plans: today_plans,wday: japanese_weekday_name(wday_num)}
+        @week_days.push(days)
     end
   end
-end
+
+
